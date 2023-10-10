7d12da8135cf7d4ace2fb16d2fae4e5ff828fac9
ArtemPopov18 <Arshawin1991gmail.com>
2023-10-10 14:16:18 +0700

    Created models from responses and configured Retrofit.

---
+++ app/src/main/java/com/example/news/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.news.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "a1bbd1e1729445049fc812903255073a"
+    }
+}
