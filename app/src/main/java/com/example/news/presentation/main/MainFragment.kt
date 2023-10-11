d49ae6ea0dcb33b50c12dca837dd32bfb0c7c256
ArtemPopov18 <Arshawin1991gmail.com>
2023-10-11 22:36:41 +0700

    Layout the Main fragment, adapter item. Create a RecyclerView. Create a repository, download data from the server, add it to the adapter

--- app/src/main/java/com/example/news/presentation/main/MainFragment.kt
+++ app/src/main/java/com/example/news/presentation/main/MainFragment.kt
@@ -1,12 +1,16 @@
 package com.example.news.presentation.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.news.databinding.FragmentMainBinding
+import com.example.news.presentation.adapters.NewsAdapter
+import com.example.news.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,6 +20,7 @@ class MainFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel by viewModels<ManeViewModel>()
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +32,33 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.all
+        initAdapter()
+        viewModel.newsLiveData.observe(viewLifecycleOwner) { responce ->
+            when(responce) {
+                is Resource.Success -> {
+                    binding.pagProgressBar.visibility = View.INVISIBLE
+                    responce.data?.let {
+                        newsAdapter.differ.submitList(it.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    binding.pagProgressBar.visibility = View.INVISIBLE
+                    responce.data?.let {
+                        Log.e("checkData", "MainFragment: error $it")
+                    }
+                }
+                is Resource.Loading -> {
+                    binding.pagProgressBar.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        newsAdapter = NewsAdapter()
+        binding.newsAdapter.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
